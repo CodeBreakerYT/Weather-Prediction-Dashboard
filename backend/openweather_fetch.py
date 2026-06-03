a452f6a2cb786ab75859af6beb2f57ad5938b351
Rishav Goswami <202582434+CodeBreakerYT@users.noreply.github.com>
2026-06-03 17:17:39 +0530

    update v0.1

--- backend/openweather_fetch.py
+++ backend/openweather_fetch.py
@@ -8,9 +8,8 @@ if not os.getenv("VERCEL"):
     env_path = Path(__file__).resolve().parent.parent / '.env'
     load_dotenv(dotenv_path=env_path)
 
-api_key = os.getenv("OPEN_WEATHER_MAP_API_KEY")
-
 def get_weather(city):
+    api_key = os.getenv("OPEN_WEATHER_MAP_API_KEY")
     url = (
         f"https://api.openweathermap.org/data/2.5/weather"
         f"?q={city}&appid={api_key}&units=metric"
